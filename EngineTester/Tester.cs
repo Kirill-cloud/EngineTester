534947f771c19e59396af527530d0e624f05cb4b
Кирилл <kir6941@gmail.com>
2021-05-06 15:08:22 +0700

    0.1

--- EngineTester/Tester.cs
+++ EngineTester/Tester.cs
@@ -9,9 +9,19 @@ namespace EngineTester
     class Tester
     {
         ITestBehavior behavior = new TemperatureTestBehavior();
-        IEngine engine = new InternalCombustionEngine(20,110);
+        IEngine engine;
         public async Task<double> StartTest() 
         {
+            double envTemp;
+            Console.WriteLine("Введите окружающую температуру");
+            while(!double.TryParse(Console.ReadLine(), out envTemp)) 
+            {
+                Console.WriteLine("Не удалось распознать");
+                Console.WriteLine("Введите окружающую температуру");
+            }
+
+            engine = new InternalCombustionEngine(envTemp, 110);
+            
             return await behavior.StartTest(engine);
         }
     }
